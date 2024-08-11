fef60f8935186a72ad443381b95c0bcf0ee01bf6
ehhe <wangting@ehhe.local>
2024-08-11 19:45:12 +0800

    Initial commit

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='my_image_plugin',
     version='0.1',
-    packages=['my_image_plugin'],
+    packages=find_packages(where='src'),  # 指定包所在的目录
+    package_dir={'': 'src'},  # 指定包目录相对于项目根目录的位置
     install_requires=[
         'dashscope',
     ],
     entry_points={
         'console_scripts': [
-            'generate-image-url=my_image_plugin.plugin:generate_image_url',
+            'generate-image-url=my_image_plugin:generate_image_url',  # 注意这里的路径
         ],
     },
 )
